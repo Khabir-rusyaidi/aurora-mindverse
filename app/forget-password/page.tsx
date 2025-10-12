6ef486563411c58bbe7d5d7524abb538e01c0291
Khabir <khabirrusyaidi@gmail.com>
2025-10-12 18:45:48 +0800

    update project

---
+++ app/forget-password/page.tsx
@@ -0,0 +1,8 @@
+export default function ForgotPasswordTest() {
+  return (
+    <div style={{minHeight:'100vh',display:'grid',placeItems:'center',fontFamily:'system-ui'}}>
+      <h1>Forgot Password ROUTE OK</h1>
+      <a href="/login" style={{marginTop:12,textDecoration:'underline'}}>Back to login</a>
+    </div>
+  );
+}
